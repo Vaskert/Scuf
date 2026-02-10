6073614d99737e43f11d5470bed7c2eb96f5264b
Vaskert <victor.askert01@gmail.com>
2026-02-10 15:43:35 +0100

    Add second batch of app directory files (dynamic routes)

---
+++ app/aktiviteter/page.tsx
@@ -0,0 +1,9 @@
+import { ActivityList } from "@/components/sections/activities/ActivityList";
+
+export default function AktiviteterPage() {
+    return (
+        <main className="min-h-screen bg-white">
+            <ActivityList />
+        </main>
+    );
+}
